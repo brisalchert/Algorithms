00cf37b26808fc9d077fc20bc8fd98224631d2df
bsalchert <salchert@hartford.edu>
2022-11-08 16:59:12 -0500

    Set up PascalTriangle.java

---
+++ Pascal/PascalTriangle.java
@@ -0,0 +1,9 @@
+//**********************************************************************
+//  PascalTriangle.java
+//
+//  Generates the nth row of Pascal's Triangle based on input n.
+//**********************************************************************
+
+public class PascalTriangle {
+    private int n;
+}
