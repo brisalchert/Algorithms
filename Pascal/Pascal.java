b391ab922fb4d4c670eda27f02dbf2f04afd995c
bsalchert <salchert@hartford.edu>
2022-11-08 17:40:24 -0500

    Created Pascal.java to get nth row of triangle

--- Pascal/Pascal.java
+++ Pascal/Pascal.java
@@ -1,5 +1,3 @@
-package Pascal;
-
 //**********************************************************************
 //  Pascal.java
 //
@@ -7,8 +5,32 @@ package Pascal;
 //  of Pascal's triangle.
 //**********************************************************************
 
+import java.util.Scanner;
+
 public class Pascal {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        int[] nthRow;
 
+        System.out.println();
+        System.out.print("\tEnter row number n: ");
+        n = input.nextInt();
+
+        nthRow = PascalTriangle.generateRow(n);
+
+        System.out.println();
+        System.out.print("\tThe " + n + "th row of Pascal's Triangle: ");
+
+        for (int index = 0; index < nthRow.length; index++) {
+            if (index < (nthRow.length - 1)) {
+                System.out.print(nthRow[index] + ", ");
+            }
+            else {
+                System.out.print(nthRow[index]);
+            }
+        }
+
+        input.close();
     }
 }
