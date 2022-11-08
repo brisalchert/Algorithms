cdbbc54c38ff462c64f1e6b39a4ce19eb73f198e
bsalchert <salchert@hartford.edu>
2022-11-08 17:40:36 -0500

    Created class PascalTriangle.java to generate rows

--- Pascal/PascalTriangle.java
+++ Pascal/PascalTriangle.java
@@ -1,5 +1,3 @@
-package Pascal;
-
 //**********************************************************************
 //  PascalTriangle.java
 //
@@ -7,5 +5,44 @@ package Pascal;
 //**********************************************************************
 
 public class PascalTriangle {
-    private int n;
+    //------------------------------------------------------------------
+    //  Generates the nth row of Pascal's Triangle.
+    //------------------------------------------------------------------
+    public static int[] generateRow(int n) {
+        int[] row = new int[n];
+
+        for (int index = 0; index < n; index++) {
+            row[index] = generateValue(n, index);
+        }
+
+        return row;
+
+    }
+
+    //------------------------------------------------------------------
+    //  Generates the ith value of the nth row of Pascal's Triangle.
+    //------------------------------------------------------------------
+    private static int generateValue(int n, int i) {
+        int rowValue;
+
+        if (n < 3) {
+            rowValue = 1;
+
+            return rowValue;
+
+        }
+
+        if (i == 0 || i == n) {
+            rowValue = 1;
+
+            return rowValue;
+
+        }
+        else {
+            rowValue = (generateValue((n - 1), (i - 1))
+                      + generateValue((n - 1), i));
+
+            return rowValue;
+        }
+    }
 }
