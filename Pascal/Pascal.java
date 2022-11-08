50acd317970ce0e077aea35b7b5305f25a2da207
bsalchert <salchert@hartford.edu>
2022-11-08 16:57:51 -0500

    Set up Pascal.java

---
+++ Pascal/Pascal.java
@@ -0,0 +1,12 @@
+//**********************************************************************
+//  Pascal.java
+//
+//  Asks the user for integer input n and then generates the nth row
+//  of Pascal's triangle.
+//**********************************************************************
+
+public class Pascal {
+    public static void main(String[] args) {
+
+    }
+}
