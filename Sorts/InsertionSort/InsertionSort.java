38218805acb43a4a5d4b0044f2875494307f59c1
Brian Salchert <brisalchert@gmail.com>
2024-04-29 11:43:35 -0400

    Created separate printList() method

--- Sorts/InsertionSort/InsertionSort.java
+++ Sorts/InsertionSort/InsertionSort.java
@@ -6,8 +6,6 @@ public class InsertionSort {
         Scanner input = new Scanner(System.in);
         int listSize;
         int[] list;
-        int valueToSort;
-        int indexToCompareTo;
 
         // Ask user for input
         System.out.print("Enter number of integers to sort: ");
@@ -22,20 +20,35 @@ public class InsertionSort {
 
         // Print unsorted list
         System.out.print("\tUnsorted List: ");
+        printList(list);
+
+        System.out.println();
+
+        // Sort the list
+        list = insertionSort(list);
+
+        // Print sorted list
+        System.out.print("\tSorted List: ");
+        printList(list);
+
+        input.close();
+    }
+
+    public static void printList(int[] list) {
         for(int i = 0; i < list.length; i++) {
-            if(i != (list.length - 1)) {
+            if (i != (list.length - 1)) {
                 System.out.print(list[i] + ", ");
             } else {
                 System.out.print(list[i]);
             }
         }
+    }
 
-        System.out.println();
-
+    public static int[] insertionSort(int[] list) {
         for(int index = 1; index < list.length; index++) {
             // Compare valueToSort to each value before it in the list
-            valueToSort = list[index];
-            indexToCompareTo = index - 1;
+            int valueToSort = list[index];
+            int indexToCompareTo = index - 1;
 
             while(indexToCompareTo >= 0 && (list[indexToCompareTo] > valueToSort)) {
                 // Swap values
@@ -45,16 +58,6 @@ public class InsertionSort {
             }
         }
 
-        // Print sorted list
-        System.out.print("\tSorted List: ");
-        for(int i = 0; i < list.length; i++) {
-            if (i != (list.length - 1)) {
-                System.out.print(list[i] + ", ");
-            } else {
-                System.out.print(list[i]);
-            }
-        }
-
-        input.close();
+        return list;
     }
 }
